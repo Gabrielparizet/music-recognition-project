e16255c46ce3c3e84c4b9cdfdd5dd14226662300
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-01 14:48:54 +0100

    (feat) Ongoing working on uploading audio file

--- javascript/test.js
+++ javascript/test.js
@@ -4,8 +4,26 @@
 //   console.log(fileList);
 // });
 
-function loadAudioFile(e){
-  const url = URL.createObjectURL(e.files[0]);
-  console.log(url);
-  document.getElementById("track").setAttribute("src", url);
-}
+
+// function loadAudioFile(e){
+//   const url = URL.createObjectURL(e.files[0]);
+//   console.log(url);
+//   document.getElementById("track").setAttribute("src", url);
+// }
+
+
+// document.getElementById("audio-file").addEventListener("change", function(){
+//   var file = this.files[0];
+//   var reader = new FileReader();
+//   reader.onload = function(event){
+//     var arrayBuffer = reader.result;
+//     console.log(arrayBuffer);
+//   };
+//   reader.readAsArrayBuffer(file);
+//   console.log(reader);
+// })
+
+// var audioContext = new AudioContext();
+// audioContext.decodeAudioData(arrayBuffer, function(decodedData){
+
+// });
